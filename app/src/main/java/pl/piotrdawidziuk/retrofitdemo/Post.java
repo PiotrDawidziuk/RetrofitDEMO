cf3c86533f4980651083235f0bb8fd3ff34adcd9
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-04 23:18:12 +0200

    JsonPlaceHolderApi

--- app/src/main/java/pl/piotrdawidziuk/retrofitdemo/Post.java
+++ app/src/main/java/pl/piotrdawidziuk/retrofitdemo/Post.java
@@ -13,5 +13,35 @@ public class Post {
     @SerializedName("body")
     private String text;
 
+    public int getUserId() {
+        return userId;
+    }
 
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
